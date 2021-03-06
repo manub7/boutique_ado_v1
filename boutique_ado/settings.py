9f5c1faf0562d7abc6bb00ead186f1d949202e63
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-03-06 18:38:47 +0000

    Added gmail to django

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -213,7 +213,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     
