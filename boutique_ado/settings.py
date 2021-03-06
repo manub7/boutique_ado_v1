1ed6378b1fe12a84e5968d95b1d50cc510b1254b
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-03-06 17:36:30 +0000

    Fixed typo for custom domain aws

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     #Bucket config
-    AWS_STORAGE_BUCKET = 'manub7-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'manub7-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
